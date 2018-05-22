7d37f084649f8d12e18905af4ff1d14033c39fc1
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-05-22 14:48:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(cheese_types)
-  cheese_types.find { |cheese_types| cheddar, gouda, camembert }
+  cheese_types.find { |cheese_types| == cheddar, gouda, camembert }
 end
